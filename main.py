55331fc1dc8eaa19c9f1731e854197e832f1f5bd
Dat Huynh Su <sudat.huynh@stud.h-da.de>
2026-08-30 04:37:21 +0200

    add: first Transformer model

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import tiktoken # byte-pair tokenizer
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-
-
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "drop_rate": 0.1, # Dropout rate
+    "qkv_bias": False # Query-Key-Value bias
+}
 
 class GPTDataset(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
@@ -24,6 +31,98 @@ class GPTDataset(Dataset):
     
     def __getitem__(self, index):
         return self.input_ids[index], self.target_ids[index] 
+        
+class AttentionLayer(torch.nn.Module):
+    def __init__(self, d_in, d_out, dropout=0.1, qkv_bias=False):
+        super(AttentionLayer, self).__init__()
+        self.W_Q = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_K = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_V = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = torch.nn.Dropout(dropout)
+    
+    def forward(self, input):
+        query = self.W_Q(input)
+        key = self.W_K(input)
+        value = self.W_V(input)
+
+        attn_score = torch.matmul(query, key.transpose(-2, -1))
+        attn_weight = self.dropout(torch.softmax(attn_score / key.shape[-1] ** 0.5, dim=-1))
+        outputs = torch.matmul(attn_weight, value)
+        return outputs
+
+
+class CausalAttentionLayer(torch.nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout=0.1, qkv_bias=False):
+        super(CausalAttentionLayer, self).__init__()
+        self.W_Q = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_K = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_V = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+        self.dropout = torch.nn.Dropout(dropout)
+    
+    def forward(self, input):
+        query = self.W_Q(input)
+        key = self.W_K(input)
+        value = self.W_V(input)
+
+        attn_score = torch.matmul(query, key.transpose(-2, -1))
+        attn_score = self.dropout(attn_score.masked_fill(self.mask.bool()[:input.shape[-2], :input.shape[-2]], -float('inf')))
+        attn_weight = torch.softmax(attn_score / input.shape[-1] ** 0.5, dim=-1)
+        outputs = torch.matmul(attn_weight, value)
+        return outputs
+    
+class MultiheadAttentionLayer(torch.nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert(d_out % num_heads == 0), \
+            "d_out muss be divisible with num_head"
+        self.d_out = d_out 
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_Q = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_K = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_V = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = torch.nn.Linear(d_out, d_out)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        ) 
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        query = self.W_Q(x)
+        key = self.W_K(x)
+        value = self.W_V(x)
+
+        # seperate the dimension to smaller num_heads with head_dim dimension
+        query = query.view(b, num_tokens, self.num_heads, self.head_dim)
+        key = key.view(b, num_tokens, self.num_heads, self.head_dim)
+        value = value.view(b, num_tokens, self.num_heads, self.head_dim)
+        
+        # transpose the dimension so that num_tokens and head_dim are 2 last layer
+        query = query.transpose(1, 2)
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
+
+        attn_score = query @ key.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_score.masked_fill_(mask_bool, -float("inf"))
+        attn_weight = torch.softmax(attn_score / num_tokens ** 0.5, dim=-1)
+
+        attn_weight = self.dropout(attn_weight)
+
+        # reverse the transpose from previous
+        context_vec = (attn_weight @ value).transpose(1, 2)
+        # the contiguous is to be sure that the memory in a block for the view to execute
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        # make the output of those head smoothout since it is just concat form those head
+        context_vec = self.out_proj(context_vec)
+        return context_vec
 
 def create_loader(txt, batch_size=4, max_length=128, stride=128, shuffle=True, drop_last=True, num_worker=0):
     tokenizer = tiktoken.get_encoding("gpt2")
@@ -40,24 +139,5 @@ def create_loader(txt, batch_size=4, max_length=128, stride=128, shuffle=True, d
 with open("the_verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_loader(raw_text, batch_size=8, max_length=4, stride = 4)
+dataloader = create_loader(raw_text, batch_size=1, max_length=4, stride = 4)
 
-vocab_size = 50257
-output_dim = 256
-
-data_iter = iter(dataloader)
-first_data = next(data_iter)
-data, target = first_data
-
-# token embedding
-# for every token there is an vector weigth with out_dim in the layer. And for every token ID we do a lookup table
-token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-token_embedding = token_embedding_layer(data)
-
-# for every pos in the context_lenght have an vector in the weight
-context_length = 4
-pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
-pos_embedding = pos_embedding_layer(torch.arange(context_length))
-
-input_embedding = token_embedding + pos_embedding
-print(input_embedding)
